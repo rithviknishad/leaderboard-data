d38bff284c832e77bbaf2d7e5d80f0cefd689344
rithviknishad <mail@rithviknishad.dev>
2023-12-19 18:01:19 +0530

    Leaderboard as submodule

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,8 +1,1 @@
-: "${LEADERBOARD_REPO:=https://github.com/coronasafe/leaderboard.git}"
-
-git init
-git remote add --mirror=fetch origin ${LEADERBOARD_REPO}
-git pull origin main
-
-ln -s ../ ./data-repo
-
+ln -s ../ leaderboard/data-repo
